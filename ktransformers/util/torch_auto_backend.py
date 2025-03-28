0556263dc88a79c0063f1e8d8c0594041406bafc
ZhouYu <yu.zhou@mthreads.com>
2025-03-28 10:50:35 +0800

    update
    Signed-off-by: ZhouYu <yu.zhou@mthreads.com>

--- ktransformers/util/torch_auto_backend.py
+++ ktransformers/util/torch_auto_backend.py
@@ -14,19 +14,22 @@ elif MUSA_HOME is not None:
     torch.cuda = torch.musa
     torch.cuda.CUDAGraph = torch.musa.MUSAGraph
 
-    # torch.cuda.nvtx.range_push = _pass_pvtx
-    # torch.cuda.nvtx.range_pop = _pass_pvtx
-    # torch.cuda.nvtx.range = _pass_pvtx
+    def _pass_pvtx(*args, **kwargs):
+        return
+
+    torch.cuda.nvtx.range_push = _pass_pvtx
+    torch.cuda.nvtx.range_pop = _pass_pvtx
+    torch.cuda.nvtx.range = _pass_pvtx
     # torch.cuda.is_available = torch.musa.is_available
     # torch.cuda.current_device = lambda : f'musa:{torch.musa.current_device()}'
     torch.cuda.device_count = torch.musa.device_count
     torch.cuda.set_device = torch.musa.set_device
-    torch.cuda.DoubleTensor = torch.musa.DoubleTensor
-    torch.cuda.FloatTensor = torch.musa.FloatTensor
-    torch.cuda.LongTensor = torch.musa.LongTensor
-    torch.cuda.HalfTensor = torch.musa.HalfTensor
-    torch.cuda.BFloat16Tensor = torch.musa.BFloat16Tensor
-    torch.cuda.IntTensor = torch.musa.IntTensor
+    # torch.cuda.DoubleTensor = torch.musa.DoubleTensor
+    # torch.cuda.FloatTensor = torch.musa.FloatTensor
+    # torch.cuda.LongTensor = torch.musa.LongTensor
+    # torch.cuda.HalfTensor = torch.musa.HalfTensor
+    # torch.cuda.BFloat16Tensor = torch.musa.BFloat16Tensor
+    # torch.cuda.IntTensor = torch.musa.IntTensor
     torch.cuda.synchronize = torch.musa.synchronize
     torch.cuda.get_rng_state = torch.musa.get_rng_state
     torch.cuda.set_rng_state = torch.musa.set_rng_state
@@ -34,8 +37,8 @@ elif MUSA_HOME is not None:
     torch.cuda.empty_cache = torch.musa.empty_cache
     torch.Tensor.cuda = torch.Tensor.musa
     torch.cuda.manual_seed = torch.musa.manual_seed
-    torch.cuda.Event = torch.musa.Event
-    torch.cuda.Stream = torch.musa.Stream
+    # torch.cuda.Event = torch.musa.Event
+    # torch.cuda.Stream = torch.musa.Stream
     torch.cuda.get_device_properties = torch.musa.get_device_properties
 
      # Memory
@@ -44,13 +47,13 @@ elif MUSA_HOME is not None:
     torch.cuda.memory_reserved = torch.musa.memory_reserved
     torch.cuda.max_memory_reserved = torch.musa.max_memory_reserved
 
-    original_empty = torch.empty
-    def patched_empty(*args, **kwargs):
-        if 'device' in kwargs and kwargs['device'] == 'cuda':
-            kwargs['device'] = 'musa'
-        result = original_empty(*args, **kwargs)
-        return result
-    torch.empty = patched_empty
+    # original_empty = torch.empty
+    # def patched_empty(*args, **kwargs):
+    #     if 'device' in kwargs and kwargs['device'] == 'cuda':
+    #         kwargs['device'] = 'musa'
+    #     result = original_empty(*args, **kwargs)
+    #     return result
+    # torch.empty = patched_empty
 
     torch.Tensor.double = torch.Tensor.float
 
